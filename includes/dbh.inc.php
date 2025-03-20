770191ef3abcf3cdd705619302cccb6027789a9a
S-Ketan <ketan10923@gmail.com>
2025-03-20 16:12:01 +0100

    Implement signup form and styling;

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,4 +1,5 @@
-<?
+<?php
+
 $dsn = 'mysql:host=locakhost;dbname=myfirstdatabase';
 $dbusername = 'root';
 $dbpassword = '';
--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Signup</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    
+    <h3>Signup</h3>
+    <form action="includes/formhandler.inc.php" method="POST">
+        <input type="text" name="username" placeholder="Username" >
+        <input type="password" name="pwd" placeholder="Password" >
+        <input type="text" name="email" placeholder="Email">
+        <button>Signup</button>
+    </form>
 </body>
 </html>
