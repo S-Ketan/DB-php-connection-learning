24a6724bb0f546d7cf8501845999564e1dbf2ee2
S-Ketan <ketan10923@gmail.com>
2025-03-23 14:11:58 +0100

    Enhance user update functionality with old password verification

--- includes/userUpdate.inc.php
+++ includes/userUpdate.inc.php
@@ -4,20 +4,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
+    $oldpwd = $_POST['oldpwd'];
 
     try {
         require_once 'dbh.inc.php';
-        $query = "UPDATE users SET username =:username, pwd = :pwd WHERE email = :email;";
+
+        $query = "SELECT pwd FROM users WHERE email = :email;";
         $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $stmt->bindParam(":username", $username);
-        $stmt->bindParam(":pwd", $pwd);
-        $stmt->bindParam("email", $email);
+        if (password_verify($oldpwd, $user["pwd"])) {
+            $query = "UPDATE users SET username =:username, pwd = :pwd WHERE email = :email;";
+            $stmt = $pdo->prepare($query);
 
-        $stmt -> execute();
-        header("Location: ../index.php");
-        exit();
+            $options=['cost'=>12];
+            $hashedpwd = password_hash($pwd, PASSWORD_BCRYPT, $options);
 
+            $stmt->bindParam(":username", $username);
+            $stmt->bindParam(":pwd", $hashedpwd);
+            $stmt->bindParam("email", $email);
+
+            $stmt->execute();
+            echo "User updated";
+            exit();
+        } else {
+            echo "Wrong password";
+            exit();
+        }
     } catch (PDOException $e) {
         die("Query Failed: " . $e->getMessage());
         ;
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,33 +8,51 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
+
 <body>
-    <h3>Signup</h3>
-    <form action="includes/formhandler.inc.php" method="POST">
-        <input type="text" name="username" placeholder="Username" >
-        <input type="password" name="pwd" placeholder="Password" >
-        <input type="text" name="email" placeholder="Email">
-        <button>Signup</button>
-    </form>
-    <h3>Change</h3>
-    <form action="includes/userUpdate.inc.php" method="POST">
-        <input type="text" name="username" placeholder=" New Username" >
-        <input type="password" name="pwd" placeholder=" New Password" >
-        <input type="text" name="email" placeholder="Email">
-        <button>Update</button>
-    </form>
-    <h3>Delete</h3>
-    <form action="includes/userDelete.inc.php" method="POST">
-        <input type="text" name="username" placeholder="Username" >
-        <input type="password" name="pwd" placeholder="Password" >
-        
-        <button>Delete</button>
-    </form>
-    <h3>Search</h3>
-    <form action="search.php" method="post">
-        <label for="search">Search for user:</label>
-        <input type="text" name="usersearch" id="search" placeholder="Search for user">
-        <button>Search</button>
-    </form>
-</body>
+    <div style="display: flex; justify-content: space-around; flex-direction: column; align-items: center; height: 100vh; gap: 20px;">
+
+        <div>
+            
+            <h3>Signup</h3>
+            <form action="includes/formhandler.inc.php" method="POST">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <input type="text" name="email" placeholder="Email">
+                <button>Signup</button>
+            </form>
+        </div>
+        <div>
+            
+            <h3>Change</h3>
+            <form action="includes/userUpdate.inc.php" method="POST">
+                <input type="text" name="username" placeholder=" New Username">
+                <input type="password" name="pwd" placeholder=" New Password">
+                <input type="password" name="oldpwd" placeholder=" Old Password">
+                <input type="text" name="email" placeholder="Email">
+                <button>Update</button>
+            </form>
+        </div>
+        <div>
+            
+            <h3>Delete</h3>
+            <form action="includes/userDelete.inc.php" method="POST">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                
+                <button>Delete</button>
+            </form>
+        </div>
+        <div>
+            
+            <h3>Search</h3>
+            <form action="search.php" method="post">
+                <label for="search">Search for user:</label>
+                <input type="text" name="usersearch" id="search" placeholder="Search for user">
+                <button>Search</button>
+            </form>
+        </div>
+    </div>
+    </body>
+
 </html>
