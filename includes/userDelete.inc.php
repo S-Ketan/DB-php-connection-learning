8fa34bfae15a595cd716e2ab8ed9dcb2f646ed55
S-Ketan <ketan10923@gmail.com>
2025-03-23 13:43:18 +0100

    Enhance user deletion functionality with password verification

--- includes/userDelete.inc.php
+++ includes/userDelete.inc.php
@@ -2,19 +2,33 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
-   
     $pwd = $_POST['pwd'];
-
+   
+    
+ 
     try {
         require_once 'dbh.inc.php';
-        $query = "DELETE FROM users WHERE username = :username AND pwd = :pwd;";
+
+        $query = "SELECT pwd FROM users WHERE username = :username;";
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(":username", $username);
-        $stmt->bindParam(":pwd", $pwd);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(password_verify($pwd, $user["pwd"])) {
 
-        $stmt -> execute();
-        header("Location: ../index.php");
-        exit();
+            
+            $query = "DELETE FROM users WHERE username = :username";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(":username", $username);
+            
+            
+            $stmt -> execute();
+            echo"User deleted";
+            exit();
+        } else {
+            echo "Wrong password";
+            exit();
+        }
 
     } catch (PDOException $e) {
         die("Query Failed: " . $e->getMessage());
